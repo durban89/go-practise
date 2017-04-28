f0af4fcfef3ad8d569b445c633f5e32094027034
durban.zhang <durban.zhang@gmail.com>
2017-04-28 08:08:38 +0800

    add sliceDefaultEx nilSliceEx CreateSliceWithMakeEx

--- practise.go
+++ practise.go
@@ -184,6 +184,57 @@ func sliceLiteralsEx() {
 	fmt.Println(s)
 }
 
+func sliceDefaultEx() {
+	fmt.Println("====================sliceDefaultEx====================")
+	s := []int{1, 2, 3, 4, 5, 6}
+
+	s = s[1:4]
+	// fmt.Println(s)
+	printSlice(s)
+
+	s = s[2:]
+	// fmt.Println(s)
+	printSlice(s)
+
+	s = s[:3]
+	// fmt.Println(s)
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d, cap=%d, %v\n", len(s), cap(s), s)
+}
+
+var nilS []int
+
+func nilSliceEx() {
+	fmt.Println(nilS)
+
+	if nilS == nil {
+		fmt.Println("nil")
+	}
+}
+
+func CreateSliceWithMakeEx() {
+	fmt.Println("====================CreateSliceWithMakeEx====================")
+	a := make([]int, 5)
+	printSlice2("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice2("b", b)
+
+	c := b[:2]
+	printSlice2("c", c)
+
+	d := c[2:5]
+	printSlice2("d", d)
+
+}
+
+func printSlice2(s string, x []int) {
+	fmt.Printf("%s, len=%d, cap=%d, %v \n", s, len(x), cap(x), x)
+}
+
 func main() {
 	sum := 0
 	for i := 0; i < 100; i++ {
@@ -218,4 +269,7 @@ func main() {
 	sliceEx()
 	slicesNameEx()
 	sliceLiteralsEx()
+	sliceDefaultEx()
+	nilSliceEx()
+	CreateSliceWithMakeEx()
 }
